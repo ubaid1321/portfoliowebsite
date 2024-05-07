34d5c8d565e223c648f3c8b44e8ed69903e83617
ubaid1321 <ubaidmir623@gmail.com>
2024-05-07 11:20:13 +0530

    impport change

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Experience from './components/Experience'
 import Skills from './components/Skills'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
-import Aos from "Aos"
+import Aos from "aos"
 import "aos/dist//aos.css"
 import { useEffect } from 'react'
 
